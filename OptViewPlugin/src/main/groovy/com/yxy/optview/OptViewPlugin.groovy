d6d224e92f3f4fce5033a164876d926e3b2f30e7
yunxiangyong <yunxiangyong@zuoyebang.com>
2021-12-27 15:06:16 +0800

    1

--- OptViewPlugin/src/main/groovy/com/yxy/optview/OptViewPlugin.groovy
+++ OptViewPlugin/src/main/groovy/com/yxy/optview/OptViewPlugin.groovy
@@ -1,34 +1,16 @@
 package com.yxy.optview
-import com.android.build.api.transform.*
-import com.android.build.gradle.internal.pipeline.TransformManager
+
+
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 
-class OptViewPlugin extends Transform implements Plugin<Project> {
+class OptViewPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        println 'Hello OptViewPlugin223'
+        println 'Hello OptViewPlugin2234'
     }
 
-    @Override
-    String getName() {
-        return "OptViewPlugin"
-    }
 
-    @Override
-    Set<QualifiedContent.ContentType> getInputTypes() {
-        return TransformManager.CONTENT_CLASS
-    }
-
-    @Override
-    Set<? super QualifiedContent.Scope> getScopes() {
-        return TransformManager.SCOPE_FULL_PROJECT
-    }
-
-    @Override
-    boolean isIncremental() {
-        return false
-    }
 }
